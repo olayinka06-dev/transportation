0513068a5e972c12b04e867a1cdf47858663fc18
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-06-09 08:52:19 -0700

    making some changes

--- src/Home Page/Hero.jsx
+++ src/Home Page/Hero.jsx
@@ -4,17 +4,17 @@ import React from 'react';
 const Hero = () => {
   return (
     <section class="hero bg-img flex md:flex-row flex-col-reverse justify-center items-center">
-      <div class="container w-full flex md:flex-row flex-col-reverse px-3 h-[70vh]">
-        <div class="md:w-1/2 w-full md:text-left text-center">
-          <h1 class="text-5xl font-bold w-full md:w-[450px] text-white">Unlock the Power of Blockchain in the <span className='text-color-secondary'>Transportation Sector</span></h1>
-          <p class="md:w-[400px] w-full my-7 text-white">
+      <div class="container w-full flex flex-col md:flex-row px-3 h-[70vh]">
+        <div class="w-full md:w-1/2">
+          <h1 class="text-5xl font-bold w-full md:w-1/2 text-white">Unlock the Power of Blockchain in the <span class="text-color-secondary">Transportation Sector</span></h1>
+          <p class="my-7 text-white">
             Experience the numerous benefits and advantages of integrating blockchain technology into your transportation business. Enhance transparency, streamline operations, and access innovative financing solutions with our tailored platform.
           </p>
         </div>
-        <div class="w-full md:w-1/2">
-        </div>
       </div>
     </section>
+
+
 
   )
 }
